19d2f8ec724a95c085a758eee6695d28efc79fa6
tushramishra2304@gmail.com <tusharmishra2304@gmail.com>
2023-04-10 19:26:50 +0530

    Initial commit

---
+++ Frontend/src/AuthPage.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const AuthPage = (props) => {
+    const onSubmit = (e) => {
+        e.preventDefault();
+        const { value } = e.target[0];
+        props.onAuth({ username: value, secret: value });
+    };
+    return (
+        <div className="background">
+            <form onSubmit={onSubmit} className="form-card">
+                <div className='form-title'>Welcome</div>
+                <div className='form-submit'>Set a username to get started</div>
+                <div className="auth">
+                    <div className="auth-label">Username</div>
+                    <input className="auth-input" />
+                    <button className="auth-button" type="submit">
+                        Enter
+                    </button>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default AuthPage
